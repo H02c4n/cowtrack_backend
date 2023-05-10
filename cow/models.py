fe2b0600eb9e5c345820848653702488287c482b
halil <svtr.hozcan@gmail.com>
2023-05-10 17:39:47 +0200

    cow models created

--- cow/models.py
+++ cow/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cow(models.Model):
+    cow_Id = models.PositiveSmallIntegerField()
+    cow_birthday = models.DateField(auto_created=False)
+
+
+
+class Vaccine(models.Model):
+    trikofiti_status_1D = models.BooleanField(blank=True, null=True)
+    trikofiti_status_14D = models.BooleanField(blank=True, null=True)
+    brd_clostridial_status_17D = models.BooleanField(blank=True, null=True)
+    brd_clostridial_status_45D = models.BooleanField(blank=True, null=True)
+    sap_status_2M = models.BooleanField(blank=True, null=True)
+    sap_status_3M = models.BooleanField(blank=True, null=True)
+    brucella_status_3M = models.BooleanField(blank=True, null=True)
+    lsd_status_4M = models.BooleanField(blank=True, null=True)
+    lsd_status_5M = models.BooleanField(blank=True, null=True)
+    brucella_status_7M = models.BooleanField(blank=True, null=True)
+    brd_clostridial_status_8M = models.BooleanField(blank=True, null=True)
+    sap_status_9M = models.BooleanField(blank=True, null=True)
+    bvd_ibr_status_12M = models.BooleanField(blank=True, null=True)
+    bvd_ibr_status_13M = models.BooleanField(blank=True, null=True)
+    sap_status_15M = models.BooleanField(blank=True, null=True)
+    lsd_status_17M = models.BooleanField(blank=True, null=True)
+    clostridial_status_20M = models.BooleanField(blank=True, null=True)
+    sap_status_21M = models.BooleanField(blank=True, null=True)
--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #my apps
+    'cow',
+
+    #3rd party apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
